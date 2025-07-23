90579b0e7d1d664c25afffcb6c2d501db6dcd845
John <86043332+shO0k@users.noreply.github.com>
2025-07-23 13:14:36 -0400

    file_crawler.py
    python: function to iterate through local file hierarchy and compute similarity to user-inputted sketch.

--- file_crawler.py
+++ file_crawler.py
@@ -1,41 +1,20 @@
 import os
 import random
+import CLIP
 
-#will eventually take in two feature profiles,
-#one comes from inputted sketch and one from the file it is being compared to
-#function determines how similar the profiles are and returns a float from 0.0 to 1.0
-#returns the euclidean distance of two random floats in (0,1) for now
-def comparison_score(sketch_profle,file_path_profile):
-        return 1 - abs(sketch_profile - file_path_profile)
-
-
-#will be the model that extracts features from file
-#input file path
-#generates random float in (0,1) for now
-def get_feature_profile():
-        return random.random()
-
-#iterates through file architecture and returns a list of pairs: (filename, filename_feature_profile)
-def file_iterator(start_path):
+#iterates through file architecture and returns a list of pairs: (filename, comparison score)
+def file_iterator(sketch_path, start_path):
         file_profiles = []
+        # iterate through file hieracrcy
         for dirpath, dirnames, filenames in os.walk(start_path):
                 for filename in filenames:
                         full_path = os.path.join(dirpath, filename)
-                        if filename.lower().endswith(('.pdf')):
-                                x = get_feature_profile()
-                                file_profiles.append((filename,x))
-
+                        # compare only to specific file types
+                        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+                                #compute similarity score from CLIP.py
+                                # eventually replace CLIP_cosine_similarity with a hybrid similarity score
+                                similarity_score = CLIP.CLIP_cosine_similarity(sketch_path, full_path)
+                                file_profiles.append((filename, similarity_score))
+        # sort pairs from highest to lowest similarity score
+        file_profiles.sort(key=lambda i: i[1], reverse=True)
         return file_profiles
-
-#takes in a list of pairs: (filename, filename_feature_profile) and returns a sorted list of pairs: (filename, probability_score)
-def output_list(list, sketch_profile):
-        result = []
-        for i in list:
-                result.append((i[0], comparison_score(sketch_profile,i[1])))
-        result.sort(key=lambda i: i[1], reverse=True)
-        return result
-
-start_path = 'file_path_here'
-sketch_profile = get_feature_profile()
-
-results = output_list(file_iterator(start_path), sketch_profile)
