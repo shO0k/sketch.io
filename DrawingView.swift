7801a546a96efea861983b215a6671f56b4d1fd1
John <86043332+shO0k@users.noreply.github.com>
2025-08-06 12:30:55 -0400

    Update DrawingView.swift

--- DrawingView.swift
+++ DrawingView.swift
@@ -20,67 +20,70 @@ struct DrawingView: View {
                                 .frame(width: 400, height: 400)
                                 .gesture(DragGesture(minimumDistance: 0)
                                         .onChanged { value in
-                                                currentDrawing.append(value.location)
+                                            let location = value.location
+                                                currentDrawing.append(location)
                                         }
-                                        .onEnded { _ in
+                                        .onEnded { value in
+                                            let location = value.location
+                                            currentDrawing.append(location)
                                             drawings.append(currentDrawing)
-                                            currentDrawing.removeAll()	
+                                            currentDrawing.removeAll()
                                             image = renderImage(from: drawings)
                                         }
                                 )
-			//show completed strokes
-			ForEach(drawings, id: \.self) { drawing in
-				Path { path in
-					guard let firstPoint = drawing.first else {return}
-					path.move(to: firstPoint)
-					for point in drawing.dropFirst() {
-						path.addLine(to: point)
-					}
-				}
-				.stroke(Color.black, lineWidth: 2)
-			}
-			// show current stroke in progress
-			Path { path in
-				guard let firstPoint = currentDrawing.first else {return}
-				path.move(to: firstPoint)
-				for point in currentDrawing.dropFirst() {
-					path.addLine(to: point)
-				}
-			}
-			.stroke(Color.gray, lineWidth: 2)
-		}
-        .border(Color.gray)
-        .onChange(of: clearTrigger) {
-            currentDrawing.removeAll()
-            drawings.removeAll()
-            image = nil
+                        //show completed strokes
+                        ForEach(drawings, id: \.self) { drawing in
+                                Path { path in
+                                        guard let firstPoint = drawing.first else {return}
+                                        path.move(to: firstPoint)
+                                        for point in drawing.dropFirst() {
+                                                path.addLine(to: point)
+                                        }
+                                }
+                                .stroke(Color.black, lineWidth: 2)
+                        }
+                        // show current stroke in progress
+                        Path { path in
+                                guard let firstPoint = currentDrawing.first else {return}
+                                path.move(to: firstPoint)
+                                for point in currentDrawing.dropFirst() {
+                                        path.addLine(to: point)
+                                }
+                        }
+                        .stroke(Color.gray, lineWidth: 2)
+                }
+       		.border(Color.gray)
+        	.onChange(of: clearTrigger) { _, _ in
+			currentDrawing.removeAll()
+            		drawings.removeAll()
+            		image = nil
+        	}
         }
-  	}
 
         func renderImage(from drawings: [[CGPoint]]) -> NSImage {
                 let imageSize = CGSize(width: 400, height: 400)
                 let image = NSImage(size: imageSize)
                 image.lockFocus()
 
-		//white background
+                //white background
                 let context = NSGraphicsContext.current?.cgContext
                 context?.setFillColor(NSColor.white.cgColor)
                 context?.fill(CGRect(origin: .zero, size: imageSize))
-               
-		 // draw path
-		context?.setStrokeColor(NSColor.black.cgColor)
+
+                 // draw path
+                context?.setStrokeColor(NSColor.black.cgColor)
                 context?.setLineWidth(2)
 
-		for drawing in drawings {
-			guard let firstPoint = drawing.first else {continue}
-			context?.beginPath()
-			context?.move(to: firstPoint)
-			for point in drawing.dropFirst() {
-                		context?.addLine(to: point)
-			}
-                	context?.strokePath()
-		}
-		
+                for drawing in drawings {
+                        guard let firstPoint = drawing.first else {continue}
+                        context?.beginPath()
+                        context?.move(to: firstPoint)
+                        for point in drawing.dropFirst() {
+                                context?.addLine(to: point)
+                        }
+                        context?.strokePath()
+                }
+
 
                 image.unlockFocus()
                 return image
