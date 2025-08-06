d3d36bc0eb89538a94c3e7a16116a6f01da35374
John <86043332+shO0k@users.noreply.github.com>
2025-08-06 12:32:55 -0400

    Update run_search.py

--- run_search.py
+++ run_search.py
@@ -1,6 +1,14 @@
 import sys
 import json
+sys.path.append("/Users/johnshook/Unix/Projects/Sketch")
 import file_crawler as fc
+
+
+def format_results(sketch_path, start_path):
+        results = fc.file_iterator(sketch_path, start_path)
+        output_lines = [f"{path}: {score * 100:.2f}%" for path, score in results]
+        return "\n".join(output_lines)
+
 
 if __name__ == "__main__":
         if len(sys.argv) < 3:
@@ -10,8 +18,4 @@ if __name__ == "__main__":
         sketch_path = sys.argv[1]
         start_path = sys.argv[2]
 
-        results = fc.file_iterator(sketch_path, start_path)
-
-        for filename, score in results:
-                percent = score * 100
-                print(f"{filename}: {percent:.2f}" + "%")
+        print(format_results(sketch_path,start_path))
